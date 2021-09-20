05bbcdda74dda4ad0e6db052ffbd4f73d20417db
Joaquin <joaquin.helio.p@hotmail.com>
2021-09-20 18:17:14 -0300

    V10 ultima version realizada (Antes de implementacion de git)

--- DVBE-001/DVBE-001.cpp
+++ DVBE-001/DVBE-001.cpp
@@ -2,19 +2,26 @@
 #include <EEPROM.h>
 #include "HX711.h"
 
-const int DOUT_PIN = A4;//A3,A2 bascula los nietos
-const int SCK_PIN = A5;
-
-#define pinState 4
-#define pinLed 5
-
+//HX711
+#define pinDt A0
+#define pinSck A1
 HX711 balanza;
-SoftwareSerial BT (2, 3);//3,4 bascula los nietos
 
-float peso, pesoAnt, pesoSig;
-float pesoEst;
-byte intentos = 1;
-byte contador = 0;
+//Bluetooth
+#define pinTx 3
+#define pinRx 4
+#define pinState 5
+SoftwareSerial BT (pinTx, pinRx);
+
+//Indicadores
+#define pinLed 2
+
+//Valores por defecto
+#define varDefault 1.0
+#define escDefault 1000.0
+#define corDefault 1.0
+#define pesoMinDefault 50
+#define tiempo 10
 
 //Parametros de configuracion
 float variacion;
@@ -22,42 +29,48 @@ float escala;
 float correccion;
 int pesoMin;
 
+//Auxiliares
+float peso, pesoAnt, pesoSig, pesoEst;
+byte condicion;
+
 void setup() {
 
+  //Inicio de puertos
   Serial.begin(9600);
   BT.begin(9600);
-  balanza.begin(DOUT_PIN, SCK_PIN);
-
-  //Recoleccion de datos EXCEL
-  /*Serial.println("CLEARDATA");
-  Serial.println("LABEL,HORA,TIEMPO,PESO,DIFERENCIA DE PESO, PESO ESTABLE");
-  Serial.println("RESETTIMER");*/
+  balanza.begin (pinDt, pinSck);
 
   //E/S
   pinMode(pinState, INPUT);
   pinMode(pinLed, OUTPUT);
 
   //Configuracion inicial de pinLed
-  digitalWrite(pinLed, HIGH);
+  digitalWrite(pinLed, LOW);
 
-  //grabar EEPROM por primera vez con valores por defecto
+  //Recoleccion de datos EXCEL
+  /*Serial.println("CLEARDATA");
+    Serial.println("LABEL,HORA,TIEMPO,PESO,DIFERENCIA DE PESO, PESO ESTABLE");
+    Serial.println("RESETTIMER");*/
+
+
+  //Grabar EEPROM por primera vez con valores por defecto
   int eepr;
   EEPROM.get(14, eepr);
   if (eepr == -1) {
-    EEPROM.put(0, 1);//variacion
-    EEPROM.put(4, 1000);//escala
-    EEPROM.put(8, 1);//correccion
-    EEPROM.put(12, 50);//peso minimo estable
+    EEPROM.put(0, varDefault);//variacion
+    EEPROM.put(4, escDefault);//escala
+    EEPROM.put(8, corDefault);//correccion
+    EEPROM.put(12, pesoMinDefault);//peso minimo estable
     EEPROM.put(14, 1);//estado eeprom
   }
 
-  //obtener valores guardados en la EEPROM
+  //Obtener valores guardados en la EEPROM
   EEPROM.get(0, variacion);
   EEPROM.get(4, escala);
   EEPROM.get(8, correccion);
   EEPROM.get(12, pesoMin);
 
-  //configuraciones iniciales de escala y tara
+  //Configuraciones iniciales de escala y tara
   balanza.set_scale(escala);
   delay(100);
   balanza.tare(10);
@@ -67,33 +80,41 @@ void setup() {
 void loop() {
 
   //Recoleccion de datos EXCEL
-  /*Serial.print("DATA,TIME,TIMER,");
-  Serial.print(peso);
-  Serial.print(",");
-  Serial.print(peso - pesoAnt);
-  Serial.print(",");
-  Serial.print("0");
-  Serial.println("");*/
+  /* Serial.print("DATA,TIME,TIMER,");
+    Serial.print(peso);
+    Serial.print(",");
+    Serial.print(peso - pesoAnt);
+    Serial.print(",");
+    Serial.print("0");
+    Serial.println("");*/
 
   //Pulsar led en el caso de estar buscando conexion BT (Solo para HM-10,HC-05)
-   /* while (digitalRead(pinState) == 0) {
-      digitalWrite(pinLed, HIGH);
-      delay(1000);
-      digitalWrite(pinLed, LOW);
-      delay(1000);
-    }*/
-  digitalWrite(pinLed, HIGH);
+  while (digitalRead(pinState) == 0) {
+    condicion = 1;
+    digitalWrite(pinLed, LOW);
+    delay(1000);
+    digitalWrite(pinLed, HIGH);
+    delay(1000);
+  }
+  digitalWrite(pinLed, LOW);
+
+  // Mensaje para configurador serial (solo se ve una vez cuando establesco conexion)
+  if (digitalRead(pinState) == 1 && condicion == 1) {
+    condicion = 0;
+    BT.println("Escriba 'm' + 'Enviar' para ingresar al menu de configuracion de balanza");
+    delay(2000);   
+  }
 
   pesoAnt = balanza.get_units(2);
   peso = balanza.get_units(2);
   pesoSig = balanza.get_units(2);
 
   //Imprime datos en el puerto BT
-  BT.print("P");
-  BT.print(round(pesoSig));
-  BT.println("Kg");
+  BT.println("P" + (String)round(pesoSig));
+  delay(tiempo);
 
-  delay(1);
+  estable();
+
   //Lee datos del puerto BT
   if (BT.available()) {
     char lectura = BT.read();
@@ -103,58 +124,49 @@ void loop() {
         balanza.tare(10);
         break;
 
-      case 'M':
-        menu();
-        break;
-
       case 'm':
         menu();
         break;
     }
   }
-  estable();
+
 }
 
 void estable() {
 
-
   if (abs(pesoAnt - peso) <= variacion && abs(pesoSig - peso) <= variacion && abs(peso) > pesoMin) {
 
-    if (contador < intentos - 1) {
-      contador ++;
-      loop();
-    }
-    contador = 0;
-
     pesoEst = peso;
+
     while (peso > pesoMin) {
 
       //Recoleccion de datos EXCEL
-      Serial.print("DATA,TIME,TIMER,");
-      Serial.print(peso);
-      Serial.print(",");
-      Serial.print("0");
-      Serial.print(",");
-      Serial.print(pesoEst);
-      Serial.println(" ,");
+      /*Serial.print("DATA,TIME,TIMER,");
+        Serial.print(peso);
+        Serial.print(",");
+        Serial.print("0");
+        Serial.print(",");
+        Serial.print(pesoEst);
+        Serial.println(" ,");*/
 
       //Imprime datos en puerto BT
-      BT.print("E");
-      BT.print(round(pesoEst));
-      BT.println("Kg");
-      delay (1);
+      BT.println("E" + (String)round(pesoEst));
+      delay (tiempo);
+
+      //Lee balanza para saber si todavia hay carga en la balanza
       peso = balanza.get_units(5);
 
-      //Lee datos del puerto BT
+      //Lee datos del puerto BT para escuchar la tara
       if (BT.available()) {
         char lectura = BT.read();
-        switch (lectura) {
 
+        switch (lectura) {
           case 'T':
             repesado();
+            estable();
             break;
 
-          case 'M':
+          case 'm':
             menu();
             break;
         }
@@ -164,12 +176,13 @@ void estable() {
 }
 
 void menu() {
+
   char dato;
 
-  BT.println("MENU\n\nA. Variacion [Kg] \nB. Peso minimo estable \nC. Establecer escala \nG. Guardar valores en EEPROM  \nR. Reestablecer valores por defecto \nS. Salir");
+  BT.println("MENU\n\na. Establecer escala \nb. Peso minimo estable \nc. Velocidad Estabilizacion \ng. Guardar valores en EEPROM  \nr. Reestablecer valores por defecto \ns. Salir");
 
-  //Limpiar buffer
-  BT.read();
+
+  BT.read(); //Limpiar Buffer
   dato = BT.read();
 
   while (!BT.available()) {
@@ -178,36 +191,34 @@ void menu() {
   dato = BT.read();
 
   switch (dato) {
-    case 'A':
-      BT.println("Variacion: ");
-      BT.println(variacion);
-      BT.println("Ingrese un valor de 0,1 a 5 o 'S' para salir");
-      variacionF();
-      break;
 
-    case 'B':
-      BT.println("Peso minimo estable: ");
-      BT.println(pesoMin);
-      BT.println("Ingrese un valor de 5 a 500 o 'S' para salir");
-      pesoMinF();
-      break;
-
-    case 'C':
-      BT.print("Escala: ");
-      BT.println(escala);
-      BT.println("Ingrese el peso conocido en Kg 'S' para salir");
+    case 'a':
+      BT.println("Escala actual: " + (String)escala );
+      BT.println("Ingrese el peso conocido en Kg 's' para salir");
       correccionPesoF();
       break;
 
-    case 'G':
+    case 'b':
+      BT.println("Peso minimo estable actual: " + (String)pesoMin);
+      BT.println("Ingrese un valor de 10 a 500 o 's' para salir");
+      pesoMinF();
+      break;
+
+    case 'c':
+      BT.println("Velocidad de Estabilizacion: " + (String)variacion);
+      BT.println("Ingrese un valor de 0,1 a 5 o 's' para salir");
+      variacionF();
+      break;
+
+    case 'g':
       eepromGuardar();
       break;
 
-    case 'R':
+    case 'r':
       eepromReset();
       break;
 
-    case 'S':
+    case 's':
       BT.println("Saliendo...\nListo!");
       loop();
       break;
@@ -221,89 +232,26 @@ void menu() {
 
 //funcion creeada para que cuando recibe varias taras en la reestabilizacion del peso no me haga un a tara a cero se logro quitando la capacidad de lectura mientras hace el repesaje
 void repesado() {
-
+  BT.read(); //Limpiar Buffer
   //Recoleccion de datos EXCEL
-  Serial.print("DATA,TIME,TIMER,");
-  Serial.print(peso);
-  Serial.print(",");
-  Serial.print(peso - pesoAnt);
-  Serial.print(",");
-  Serial.print("0");
-  Serial.println("");
+  /* Serial.print("DATA,TIME,TIMER,");
+    Serial.print(peso);
+    Serial.print(",");
+    Serial.print(peso - pesoAnt);
+    Serial.print(",");
+    Serial.print("0");
+    Serial.println("");*/
 
   pesoAnt = balanza.get_units(2);
   peso = balanza.get_units(2);
   pesoSig = balanza.get_units(2);
 
   //Imprime datos en el puerto BT
-  BT.print("P");
-  BT.print(round(pesoSig));
-  BT.println("Kg");
+  BT.println("P" + (String)round(pesoSig));
+  delay(tiempo);
 
-  delay(1);
-
-  while(BT.available()){
-  BT.read();
-  }
-}
-
-void variacionF() {
-
-  String str;
-  float variacionAux = 0;
-
-  //limpiar buffer
-  BT.read();
-
-  while (!BT.available()) {
-  }
-
-  str = BT.readStringUntil('\n');
-  char strAux = str.charAt(0);
-  variacionAux = str.toFloat();
-
-  if (strAux == 'S' || strAux == 's') {
-    menu();
-  } else if (variacionAux >= 0.1  && variacionAux <= 5) {
-    variacion = variacionAux;
-    BT.println("Cambio correcto\nVariacion: ");
-    BT.println(variacion);
-    BT.println();
-    menu();
-  } else {
-    BT.println("Valor incorrecto");
-    BT.println("Ingrese un valor de 0,1 a 5");
-    variacionF();
-  }
-}
-
-void pesoMinF() {
-
-  String str;
-  float pesoMinAux = 0;
-
-  //limpiar buffer
-  BT.read();
-
-  while (!BT.available()) {
-  }
-
-  str = BT.readStringUntil('\n');
-  char strAux = str.charAt(0);
-  pesoMinAux = str.toInt();
-
-  if (strAux == 'S' || strAux == 's') {
-    menu();
-  } else if (pesoMinAux >= 10  && pesoMinAux <= 500) {
-    pesoMin = pesoMinAux;
-    BT.println("Cambio correcto\nPeso minimo estable: ");
-    BT.println(pesoMin);
-    BT.println();
-    menu();
-  } else {
-    BT.println("Valor incorrecto");
-    BT.println("Ingrese un valor de 10 a 500");
-    pesoMinF();
+  while (BT.available()) {
+    BT.read();
   }
 }
 
@@ -314,7 +262,9 @@ void correccionPesoF() {
   float lecturaAux;
   float promedio = 0;
 
-  //limpiar buffer
+
+  BT.read(); //limpiar buffer
+  BT.read();
   BT.read();
 
   while (!BT.available()) {
@@ -324,9 +274,9 @@ void correccionPesoF() {
   char strAux = str.charAt(0);
   correccionAux = str.toFloat();
 
-  if (strAux == 'S' || strAux == 's') {
+  if (strAux == 's') {
     menu();
-  } else if (correccionAux >= 1 && correccionAux <= 1500) {
+  } else if (correccionAux >= 1 && correccionAux <= 5000) {
 
     correccion = correccionAux;
 
@@ -339,15 +289,73 @@ void correccionPesoF() {
     balanza.set_scale(nuevaEscala);
     escala = nuevaEscala;
 
-    BT.print("Nueva escala establecida: ");
-    BT.println(nuevaEscala);
+    BT.println("Nueva escala establecida: " + (String)nuevaEscala + "\n");
     delay(1500);
-    BT.println();
     menu();
+
   } else {
-    BT.println("Valor incorrecto");
-    BT.println("Ingrese un valor de 1 a 1500");
+    BT.println("Valor incorrecto\nIngrese un valor de 1 a 5000");
+    correccionPesoF();
+  }
+}
+
+void pesoMinF() {
+
+  String str;
+  float pesoMinAux = 0;
+
+  BT.read(); //limpiar buffer
+  BT.read();
+  BT.read();
+
+  while (!BT.available() && digitalRead(pinState) == 1) {
+  }
+
+  str = BT.readStringUntil('\n');
+  char strAux = str.charAt(0);
+  pesoMinAux = str.toInt();
+
+  if (strAux == 'S' || strAux == 's') {
+    menu();
+
+  } else if (pesoMinAux >= 10  && pesoMinAux <= 500) {
+    pesoMin = pesoMinAux;
+    BT.println("Cambio correcto\nPeso minimo estable: " + (String)pesoMin + "\n");
+    menu();
+
+  } else {
+    BT.println("Valor incorrecto\nIngrese un valor de 10 a 500");
     pesoMinF();
+  }
+}
+
+void variacionF() {
+
+  String str;
+  float variacionAux = 0;
+
+  BT.read(); //limpiar buffer
+  BT.read();
+  BT.read();
+
+  while (!BT.available()) {
+  }
+
+  str = BT.readStringUntil('\n');
+  char strAux = str.charAt(0);
+  variacionAux = str.toFloat();
+
+  if (strAux == 'S' || strAux == 's') {
+    menu();
+
+  } else if (variacionAux >= 0.1  && variacionAux <= 5) {
+    variacion = variacionAux;
+    BT.println("Cambio correcto\nVariacion: " + (String)variacion + "\n");
+    menu();
+
+  } else {
+    BT.println("Valor incorrecto\nIngrese un valor de 0,1 a 5");
+    variacionF();
   }
 }
 
@@ -355,8 +363,13 @@ void eepromGuardar() {
 
   char confirmacion;
 
-  BT.println("¿Desea guardar valores en la EEPROM S|N?");
-  confirmacion = BT.read();
+
+  BT.println("¿Desea guardar valores en la EEPROM s|n?");
+  
+  BT.read(); //limpiar buffer
+  delay(2000);
+  BT.read();
+
 
   while (!BT.available()) {
   }
@@ -364,7 +377,7 @@ void eepromGuardar() {
   confirmacion = BT.read();
 
   switch (confirmacion) {
-    case 'S':
+    case 's':
 
       EEPROM.put(0, variacion);
       EEPROM.put(4, escala);
@@ -376,7 +389,7 @@ void eepromGuardar() {
       menu();
       break;
 
-    case 'N':
+    case 'n':
       BT.println("Valores no guardados");
       menu();
       break;
@@ -392,20 +405,23 @@ void eepromReset() {
 
   char confirmacion;
 
-  BT.println("¿Desea reestablecer valores por defecto S|N?");
-  confirmacion = BT.read();
-
+  BT.println("¿Desea reestablecer valores por defecto s|n?");
+  
+  BT.read(); //limpiar buffer
+  delay(2000);
+  BT.read();
+ 
   while (!BT.available()) {
   }
 
   confirmacion = BT.read();
 
   switch (confirmacion) {
-    case 'S':
-      EEPROM.put(0, 1);//variacion
-      EEPROM.put(4, 1000);//escala
-      EEPROM.put(8, 1);//correccion
-      EEPROM.put(12, 50);//peso minimo estable
+    case 's':
+      EEPROM.put(0, varDefault);//variacion
+      EEPROM.put(4, escDefault);//escala
+      EEPROM.put(8, corDefault);//correccion
+      EEPROM.put(12, pesoMinDefault);//peso minimo estable
 
       EEPROM.get(0, variacion);
       EEPROM.get(4, escala);
@@ -418,7 +434,7 @@ void eepromReset() {
       menu();
       break;
 
-    case 'N':
+    case 'n':
       BT.println("Valores no reestablecidos");
       menu();
       break;
